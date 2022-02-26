5fd7846d7474de8170307720e083e5b317d2c6aa
Marcin Barszcz <marcin.barszcz@op.pl>
2022-02-26 20:39:07 +0100

    Połączenie z baza danych

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const helmet = require('helmet');
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 app.use((req, res, next) => {
   const errpr = new Error('Strona o podanym adresie nie istnieje');
 });
+
+mongoose.set('useUnifiedTopology', true);
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('Connection failed', error);
+  });
