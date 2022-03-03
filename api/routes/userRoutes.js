585cd66f414e65d6fe539417a052cc04b2b81e27
Agnieszka Kapelanczyk <agnieszka.kapelanczyk.ak@gmail.com>
2022-03-03 14:50:24 +0100

    poprawienie sciezek

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const userController = require('../controllers/userControllers')
-const auth = require('../middlewares/userMiddlewares')
+const auth = require('../middleware/userMiddlewares')
 
 router.get('/:id', userController.getUser)
 router.post('/register', userController.registration)
