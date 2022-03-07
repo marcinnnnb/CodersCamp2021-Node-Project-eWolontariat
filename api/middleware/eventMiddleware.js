f3229f8db360ad72eb7b34f68f44e94bf079940f
Agnieszka Bury <ag.grala@gmail.com>
2022-03-07 18:37:01 +0100

    event-middleware

---
+++ api/middleware/eventMiddleware.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+
+exports.isLoggedUser =  (req, res, next) => {
+    const token = req.header('auth-token');
+    if (!token) return res.status(401).send('Access Denied. You have to log in!');
+
+    try {
+       const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+       req.user = verified;
+       
+    }
+    catch (error) {
+        res.status(400).send("Invalid Token");
+    }
+    next();
+}
