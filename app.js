25a416b4aa60709605057cdab3a25f0396cf2563
Agnieszka Kapelanczyk <agnieszka.kapelanczyk.ak@gmail.com>
2022-03-07 23:07:17 +0100

    dodano swagger do user

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const helmet = require('helmet');
 const express = require('express');
-const app = express();
+const swaggerUi = require('swagger-ui-express')
+swaggerDocument = require('./swagger.json')
+//const swaggerJsDoc = require('swagger-jsdoc')
 const eventRouter = require("./api/routes/eventRoutes");
 const categoryRouter = require("./api/routes/categoryRoutes");
 const Event = require("./api/models/eventModel");
@@ -10,6 +12,36 @@ const bodyParser = require("body-parser");
 const EventController = require('./api/controllers/eventController');
 const UserController = require('./api/controllers/userControllers');
 const userRouter = require("./api/routes/userRoutes");
+
+const port = process.env.PORT || 3000;
+
+/*const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title:"e-Wolontariat API",
+      version: "1.0.0",
+      description: "Aplikacja pomagająca w znalezieniu wolontariuszy."
+    },
+    servers: [
+      {
+        url: "http://localhost:3000"
+      }
+    ],
+  },
+  apis:["./api/routes/*.js"]
+}
+
+//const specs = swaggerJsDoc(options)*/
+
+const app = express();
+
+app.use(
+  '/api-docs',
+  swaggerUi.serve, 
+  swaggerUi.setup(swaggerDocument)
+);
+
 
 dotenv.config();
 
@@ -29,8 +61,8 @@ app.use('/user', userRouter);
 app.use('/event', eventRouter);
 app.use('/category', categoryRouter);
 
+app.listen(port, () => console.log(`Listening on port ${port}...`));
 
-
-module.exports = app;
+//module.exports = app;
 
 
