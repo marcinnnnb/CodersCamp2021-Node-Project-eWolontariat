22178ca3af51cccfd27597481a6bc938e5630ecc
DariuszKny <nasz_email_do_github>
2022-03-08 18:39:27 +0100

    Dodano filtrowanie wolontariuszy

--- api/models/commentsModel.js
+++ api/models/commentsModel.js
@@ -15,17 +15,6 @@ const commentSchema = new mongoose.Schema({
     date:{
         type: Date, 
         default: Date.now
-<<<<<<< HEAD
-=======
-    },
-    event: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref:'Event'
-    },
-    volunteer:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref:'Volunteer'
->>>>>>> 8c32bf2c8598fa670b582ace7664c6e917304c63
     }
 })
 
