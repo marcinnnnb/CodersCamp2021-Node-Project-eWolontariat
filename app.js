faf6ca10472faedc29079feca9e5381c6e219194
Agnieszka Bury <ag.grala@gmail.com>
2022-03-18 15:48:14 +0100

    update cors

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
 
 const corsOptions = {
   exposedHeaders: ['Auth-Token'],
-  allowedHeaders: ["Accept","Accept-Language","Content-Language",'Auth-Token', "x-requested-with", "authorization", "content-type"],
+  allowedHeaders: ["*","Accept","Accept-Language","Content-Language",'Auth-Token', "x-requested-with", "authorization", "content-type"],
   origin: '*'
 };
 
